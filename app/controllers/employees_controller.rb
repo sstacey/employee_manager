c871949092cdfda158aa52166bb57b497db2a3e5
sstacey <shaun.stacey1@gmail.com>
2021-10-12 15:55:42 -0500

    Add org_id to strong params

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -45,6 +45,6 @@ class EmployeesController < ApplicationController
 
   private
   def employee_params
-    params.require(:employee).permit(:first_name, :last_name, :job_id)
+    params.require(:employee).permit(:first_name, :last_name, :job_id, :org_id)
   end
 end
