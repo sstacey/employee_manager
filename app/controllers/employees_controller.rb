b390b92c2a9e939a033a4e99371c991af18a41e8
sstacey <shaun.stacey1@gmail.com>
2021-10-12 13:49:35 -0500

    Add create employee actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,5 +6,23 @@ class EmployeesController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
   end
-  
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to @employee
+    else
+      render :new
+    end
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name)
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,6 @@
 class Employee < ApplicationRecord
-
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   def full_name
     "#{first_name} #{last_name}".titleize
   end
