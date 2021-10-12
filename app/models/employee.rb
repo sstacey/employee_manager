b100c5f7758b6fa0d8679931ef1c27c0fe090dee
sstacey <shaun.stacey1@gmail.com>
2021-10-12 15:37:48 -0500

    Add job and org seeds and update formt o sort order collection'

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,6 +3,8 @@ class Employee < ApplicationRecord
   belongs_to :org
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :job_id, presence: true
+  validates :org_id, presence: true
   def full_name
     "#{first_name} #{last_name}".titleize
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
+# Seed Org Data
+Org.create(code: 'Dallas', name: 'Dallas')
+Org.create(code: 'La Cruces', name: 'Las Cruces')
+Org.create(code: 'Savannah', name: 'Savannah')
+
+# Seed Job Data
+Job.create(code: 'PB', name: 'Personal Banker')
+Job.create(code: 'Sup', name: 'Supervisor')
+Job.create(code: 'OM', name: 'Operations Managers')
+
 # Seed employee data
 100.times do |n|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
-  Employee.create(first_name: first_name, last_name: last_name)
+  job_id = rand(0..2)
+  org_id = rand(0..2)
+  Employee.create(first_name: first_name, last_name: last_name, job_id: job_id, org_id: org_id)
 end
