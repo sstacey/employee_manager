a71362490f11ecef8a44edb3498e61e4996712e1
sstacey <shaun.stacey1@gmail.com>
2021-10-12 13:56:42 -0500

    Add update actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -21,6 +21,21 @@ class EmployeesController < ApplicationController
     end
   end
 
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render :edit
+    end
+  end
+  
+
   private
   def employee_params
     params.require(:employee).permit(:first_name, :last_name)
