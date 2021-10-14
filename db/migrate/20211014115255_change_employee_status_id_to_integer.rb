dea38d258c5ef12e352db7bdbc1fe7a28e50e32c
sstacey <shaun.stacey1@gmail.com>
2021-10-14 08:54:10 -0500

    Adjust migration for emp status id to work with PG

--- db/migrate/20211014115255_change_employee_status_id_to_integer.rb
+++ db/migrate/20211014115255_change_employee_status_id_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeEmployeeStatusIdToInteger < ActiveRecord::Migration[6.1]
   def change
-    change_column :employees, :employee_status_id, :integer
+    change_column :employees, :employee_status_id, 'integer USING CAST(employee_status_id AS integer)'
   end
 end
