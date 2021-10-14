a4a1886a6f42228274a06e85171b67d4adbd552e
sstacey <shaun.stacey1@gmail.com>
2021-10-14 09:10:28 -0500

    Fix lower(int) in employee search for PG

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -15,7 +15,7 @@ class Employee < ApplicationRecord
 
   def self.search(search)
     if search
-      where('lower(first_name) LIKE :search OR lower(last_name) LIKE :search OR lower(code) LIKE :search',
+      where('lower(first_name) LIKE :search OR lower(last_name) LIKE :search OR code LIKE :search',
             search: "#{search.downcase}%")
     else
       all
