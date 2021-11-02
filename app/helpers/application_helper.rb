974fe39bac7fc4d04e875b19f01671255ca706d2
sstacey <shaun.stacey1@gmail.com>
2021-11-02 07:18:43 -0500

    Add sorting to employee statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 module ApplicationHelper
-  def sort_link(column:, label:)
+  def sort_link(column:, label:, table:)
     if column == params[:column]
-      link_to(label, employees_path(column: column, direction: next_direction))
+      # link_to(label, employees_path(column: column, direction: next_direction))
+      link_to(label, eval("#{table}_path(column: column, direction: next_direction)"))
     else
-      link_to(label, employees_path(column: column, direction: 'asc'))
+      # link_to(label, employees_path(column: column, direction: 'asc'))
+      link_to(label, eval("#{table}_path(column: column, direction: 'asc')"))
+
     end
   end
 
